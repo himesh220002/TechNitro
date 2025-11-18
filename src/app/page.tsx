8be49c801e7bf2c6a633d36518fa9e64152c6162
Himesh Satyam <satyamhimesh@gmail.com>
2025-11-18 22:07:08 +0530

    baseUrl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,11 @@ import Carousel from '@/components/Carousel'
 import FeaturedHero from '@/components/FeaturedHero'
 import ProductBlockGrid from '@/components/ProductBlockGrid'
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+
+
 async function getProducts(): Promise<Product[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, {
+  const res = await fetch(`${baseUrl} /api/products`, {
     next: { revalidate: 60 },
   })
   return res.json()
