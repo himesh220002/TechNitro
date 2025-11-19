dba5059bb758e32ebab45dbb1c536f9c2afe6176
Himesh Satyam <satyamhimesh@gmail.com>
2025-11-19 17:55:42 +0530

    a

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,5 +1,3 @@
 export const baseUrl =
-  process.env.NEXT_PUBLIC_SITE_URL ??
-  (process.env.NODE_ENV === "production"
-    ? ""
-    : "http://localhost:3000");
+  process.env.NEXT_PUBLIC_SITE_URL ||
+  "http://localhost:3000";
